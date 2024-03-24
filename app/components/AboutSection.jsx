3bbc908c5124e08f7de0b008e9b484dcd581b439
Shahabas Mohammed <shahabasg1@gmail.com>
2024-03-24 16:30:36 +0530

    imageNameChanged

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -17,7 +17,7 @@ function AboutSection() {
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image
-          src="/images/hero-image sample.png"
+          src="/images/heroImage.png"
           className=" rounded-sm h-2/3 "
           width={500}
           height={200}
